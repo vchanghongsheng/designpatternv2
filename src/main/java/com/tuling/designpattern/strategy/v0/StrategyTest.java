69c014824b562757655e92678ac5243d81aa83fe
changhs <changhs@glodon.com>
2023-11-16 16:44:11 +0800

    first

---
+++ src/main/java/com/tuling/designpattern/strategy/v0/StrategyTest.java
@@ -0,0 +1,57 @@
+package com.tuling.designpattern.strategy.v0;
+
+/**
+ * 策略模式
+ */
+public class StrategyTest {
+
+
+    public static void main(String[] args) {
+        Context context = new Context(new OperationAdd());
+        System.out.println(context.executeStrategy(1, 2));
+
+        context = new Context(new OperationSubstract());
+        System.out.println(context.executeStrategy(1, 2));
+    }
+
+}
+
+
+interface Strategy {
+    public int doOperation(int num1, int num2);
+}
+
+
+class OperationAdd implements Strategy {
+    @Override
+    public int doOperation(int num1, int num2) {
+        return num1 + num2;
+    }
+}
+
+
+class OperationSubstract implements Strategy {
+    @Override
+    public int doOperation(int num1, int num2) {
+        return num1 - num2;
+    }
+}
+
+class Context {
+    private Strategy strategy;
+
+    public Context(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public int executeStrategy(int num1, int num2) {
+        return strategy.doOperation(num1, num2);
+    }
+}
+
+
+
+
+
+
+
